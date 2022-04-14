ef8fb630795d0d32b95287b0d5109d4d60dfb5b4
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-04-14 11:40:46 -0300

    change key api

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
 const api =
-  'https://api.hgbrasil.com/weather?woeid=455825&format=json-cors&key=ee4d74a8&locale=pts'
+  'https://api.hgbrasil.com/weather?woeid=455825&format=json-cors&key=05d137b7&locale=pts'
 
 export default api
